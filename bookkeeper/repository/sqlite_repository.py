1cd10b112dc7121a61181eb3098b0cd263377dba
Mikail Gubanov <yainformal@ya.ru>
2023-02-22 22:25:25 +0200

    1. обновил метод delete - удалены кавычки параметра 'pk'. 2. написана заготовка для метода get_all c заглушкой 3. реализован метод -update v.1.0

--- bookkeeper/repository/sqlite_repository.py
+++ bookkeeper/repository/sqlite_repository.py
@@ -1,6 +1,7 @@
 """
 Модуль описывает репозиторий использующий в качестве СУБД SQLite
 """
+from abc import ABC
 
 from bookkeeper.repository.abstract_repository import AbstractRepository, T
 
@@ -10,12 +11,15 @@ from inspect import get_annotations
 import sqlite3 as sql
 
 
-class SQliteRepository(AbstractRepository[T]):
+# noinspection PyMethodParameters
+class SQliteRepository(AbstractRepository[T], ABC):
     """
    Репозиторий хранящий данные приложения с использованием СУБД SQLite.
     """
 
     def __init__(db_file: str, self, cls: type) -> None:
+        db_file.table_name = None
+        db_file.fields = None
         self.db_file = db_file
         self.table_name = cls.__name__.lower()
         self.fields = get_annotations(cls, eval_str=True)
@@ -39,5 +43,20 @@ class SQliteRepository(AbstractRepository[T]):
 
     # TODO: добавить функцию get с поддержкой необходимых типов
 
-    def delete(self, obj: T) -> None:
+    def get_all(self, where: dict[str, Any] | None = None) -> list[T]:
+        pass # TODO: Реализовать метод get all
+
+
+
+    def delete(self, pk: int) -> None:
         self.fields.pop(pk)
+
+    def update(self, obj: T) -> None:
+        if obj.pk == 0:
+            raise ValueError('attempt to update object with unknown primary key')
+
+        with sql.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute(f"UPDATE {self.table_name} SET ({obj.pk}) == ({obj})")
+        con.close()
+
